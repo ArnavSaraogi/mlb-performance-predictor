f0a514c01bb3a94522a03ff97da50e2029dbabd3
arnsar <arnavsaraogi1@gmail.com>
2025-01-08 23:37:04 -0500

    started regression model

--- python/analysis.py
+++ python/analysis.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
- 
 
 X = pd.read_csv('out.csv')[['xbadiff (2023)', 'xslgdiff (2023)', 'wobadiff (2023)']]
 y = pd.read_csv('out.csv')[['ops_change 2023-2024']]
